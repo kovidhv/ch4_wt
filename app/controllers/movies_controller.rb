384cf964090f9fa788515e6efe79cf2c8699bbed
kovidhv <kvegesna@uncc.edu>
2017-10-12 14:23:06 +0000

    Done with section 4.3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-   
+    
   def show
   id = params[:id] # retrieve movie ID from URI route
   @movie = Movie.find(id) # look up movie by unique ID
