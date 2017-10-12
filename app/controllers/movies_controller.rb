60c4847c852798f781be89c77e6b31324d02dee2
kovidhv <kvegesna@uncc.edu>
2017-10-12 14:28:49 +0000

    Done with section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,7 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
-  
+   
   def edit
     @movie = Movie.find params[:id]
   end
